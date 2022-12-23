e24717948bcaacbdedcb2c7ff10c7ecd80f5c8e5
AllanGd <allan_gd@hotmail.com>
2022-12-23 00:39:08 -0300

    create banner, form and home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
 
 <template>
+  <Banner></Banner>
   <div class="main-container">
-    <h1>Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <burger-form />
   </div>
   
 </template>
+
+<script>
+import Banner from '../components/Banner.vue';
+import BurgerForm from '../components/BurgerForm.vue';
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+    BurgerForm
+  }
+}
+</script>
